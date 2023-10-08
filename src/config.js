bb705f0732271f4627584f6df3d111be7a9500e7
Chandika Rathnayake <32993652+chandikaRT@users.noreply.github.com>
2023-10-08 16:12:41 +0530

    init

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const config = {
+    blogName: "Blog",
+    blogDescription: "Sharing what I learn as a DevOps and Pentester",
+    authorName: "Chandika Rathnayake",
+    authorDescription:
+      "aspiring DevOps and Pentester in the making",
+    authorTwitter: "https://twitter.com/",
+  
+    dev: {
+      postsdir: "./content",
+      outdir: "./public"
+    }
+  };
+  
+  module.exports = config;
